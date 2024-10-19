bc85760d951653f25cca0e7f1ae3873020f6d42e
yuyanget <845262968@qq.com>
2024-10-19 20:14:28 +0800

    release 1.0.1发布

---
+++ xy_stdio_request_base/ModuleData.py
@@ -0,0 +1,20 @@
+# -*- coding: UTF-8 -*-
+__author__ = "余洋"
+__doc__ = "ModuleData"
+"""
+  * @File    :   ModuleData.py
+  * @Time    :   2023/06/03 10:36:13
+  * @Author  :   余洋
+  * @Version :   1.0
+  * @Contact :   yuyang.0515@qq.com
+  * @License :   (C)Copyright 2019-2023, 希洋 (Ship of Ocean)
+  * @Desc    :   None
+"""
+
+from importlib_resources import files
+import xy_stdio_request_base
+
+
+class ModuleData:
+    def __init__(self):
+        self.data_path = files(xy_stdio_request_base.__name__).joinpath("data")
